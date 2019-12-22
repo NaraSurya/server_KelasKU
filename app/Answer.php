6b6bc2199dbd01a19de96812d54e89503f4c6b8e
Nara Surya <ibagungnsd13@gmail.com>
2019-12-22 21:52:50 +0800

    question error

--- app/Answer.php
+++ app/Answer.php
@@ -8,6 +8,14 @@ class Answer extends Model
 {
     protected $table = "question_user";
 
+    protected $fillable = [
+        'question_id', 'user_id', 'score','path' , 'answer'
+    ];
+
+    public $timestamps = false;
+
+
+
     public function user()
     {
         return $this->belongsTo('App\User');
